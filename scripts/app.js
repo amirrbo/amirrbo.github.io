df31e88169d19ed3da6d75b0e3d52275253414aa
Amirrbo <bordbaramirreza705@gmali.com>
2024-07-11 19:14:04 +0330

    update site

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+let menu = document.querySelector(".menu")
+let menuBtn = document.querySelector(".header-icon-bar")
+
+menuBtn.addEventListener("click",function (){
+    if (menuBtn.classList.contains("header-icon-bar")){
+        menu.style.right = "0"
+    } else {
+
+    }
+})
