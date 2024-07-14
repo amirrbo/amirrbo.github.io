e4e8f0301d301f549fa4869dcaeeed64311f5583
Amirrbo <bordbaramirreza705@gmali.com>
2024-07-14 16:58:11 +0330

    update menu mobile

--- scripts/app.js
+++ scripts/app.js
@@ -2,9 +2,5 @@ let menu = document.querySelector(".menu")
 let menuBtn = document.querySelector(".header-icon-bar")
 
 menuBtn.addEventListener("click",function (){
-    if (menuBtn.classList.contains("header-icon-bar")){
-        menu.style.right = "0"
-    } else {
-
-    }
+    menu.classList.toggle('active');
 })
