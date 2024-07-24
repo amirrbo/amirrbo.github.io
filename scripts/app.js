316e83aa36fe2b7efe2f758d8ba4ccc3d229e4dc
Amirrbo <bordbaramirreza705@gmali.com>
2024-07-24 19:27:53 +0330

    update site

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,3 @@ let menuBtn = document.querySelector(".header-icon-bar");
 menuBtn.addEventListener("click", function () {
   menu.classList.toggle("active");
 });
-
-function loader() {
-  document.getElementById("spinner").style.display = "none";
-}
