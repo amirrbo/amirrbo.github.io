707311156ffd481ee25383fea3fee6fa063a8f7f
Amirrbo <bordbaramirreza705@gmali.com>
2024-07-24 18:55:55 +0330

    Added loader

--- scripts/app.js
+++ scripts/app.js
@@ -4,3 +4,7 @@ let menuBtn = document.querySelector(".header-icon-bar");
 menuBtn.addEventListener("click", function () {
   menu.classList.toggle("active");
 });
+
+function loader() {
+  document.getElementById("spinner").style.display = "none";
+}
